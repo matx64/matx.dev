fb789c61f4ebbc7eab0acef837e962c9b346eb1d
Matheus Alves <mathalves@proton.me>
2025-05-17 12:29:30 -0300

    create dist folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
+use std::{fs, path::Path};
+use fs_extra::{copy_items, dir::{create, CopyOptions}};
 use minijinja::{context, Environment};
 
 fn main() {
-    let mut env = Environment::new();
-    
-    env.add_template("layout.html", include_str!("../templates/layout.html")).unwrap();
-    env.add_template("index.html", include_str!("../templates/index.html")).unwrap();
+    let dist_path = Path::new("dist");
+    let static_path = Path::new("static");
 
-    let template = env.get_template("index.html").unwrap();
-    println!("{}", template.render(context! {title => "matx.dev"}).unwrap());
+    create(dist_path, true).unwrap();
+    copy_items(&[static_path], dist_path, &CopyOptions::new()).unwrap();
+
+    let mut env = Environment::new();
+    env.add_template("layout.html", include_str!("../templates/layout.html")).unwrap();
+
+    fs::write(Path::new("dist/index.html"), env.render_str(include_str!("../templates/index.html"), context! {title => "matx.dev"}).unwrap()).unwrap();
 }
