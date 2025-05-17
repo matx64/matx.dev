b5c0edf6fc12a637638c6a77e2b27795d5ea42e8
Matheus Alves <mathalves@proton.me>
2025-05-17 11:46:20 -0300

    render index

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use minijinja::{context, Environment};
+
+fn main() {
+    let mut env = Environment::new();
+    
+    env.add_template("layout.html", include_str!("../templates/layout.html")).unwrap();
+    env.add_template("index.html", include_str!("../templates/index.html")).unwrap();
+
+    let template = env.get_template("index.html").unwrap();
+    println!("{}", template.render(context! {title => "matx.dev"}).unwrap());
+}
